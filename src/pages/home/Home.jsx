377244137b4946ae6fc7e6b3e588930ceed17210
BaraahCh <baraahcharmand@gmail.com>
2023-02-12 20:05:48 +0300

    fix home page design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,28 +4,76 @@ import metro from '../../images/metro.png';
 import calendar from '../../images/calendar.png';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-export default function Home() {
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-        <div>
-            <div className='d-flex flex-row justify-content-between'>
-                <div className='homelogo'>
-                    <span className="logo"><span className='blueFont'>Metro</span>way</span>
-                    <span className=" hello blueBackground">Hello Travellers</span>
-                    <span className="title1">made your booking<br /> experience easy!</span><br />
-                    <span className='description'>Train booking is a process of choosing and purchasing train seats online.<br /> It is an easy process but were are here to make it much better & simple.</span>
-                    <form className='searchform' action="">
-                        <input className='inputstyle1' type="text" placeholder='NDLS, New Delhi Railway Staion' />
-                        <input className='inputstyle2' type="text" placeholder='LJN,Lucknow Junction' />
-                        <DatePicker className="date" selected={startDate} onChange={(date) => setStartDate(date)} dateFormat="MMMM d, yyyy" />
-                        <button className="buttonstyle" >Search for trains</button>
-                    </form>
-                </div>
-                <div>
-                    <img className='imgstyle' src={metro} alt="metro" />
+import { Container, Row, Col } from "react-bootstrap";
+import Logo from "../../components/logo/Logo";
+import { useNavigate } from "react-router-dom";
 
-                </div>
-            </div>
-        </div >
+export default function Home() {
+    const navigate = useNavigate();
+    const [date, setDate] = useState(new Date());
+    const [departure, setDeparture] = useState("");
+    const [destination, setDestination] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!departure || !destination) {
+            alert("All fields are required!");
+            return;
+        }
+        navigate("/results?" + "departure=" + departure + "&destination=" + destination + "&date=" + date);
+    }
+    return (
+
+
+        <Row className="homeContainer">
+
+            <Col>
+                <div className='homelogo'>
+                    <Logo />
+                    <span className=" hello blueBackground">Hello Travellers</span>
+                    <div className="title1">made your booking experience easy!</div><br />
+                    <span className='description'>Train booking is a process of choosing and purchasing train seats online. It is an easy process but were are here to make it much better & simple.</span>
+
+                    <form className='searchform' onSubmit={handleSubmit}>
+
+                        <Row>
+                            <Col>
+                                <input
+                                    className='inputstyle'
+                                    type="text"
+                                    placeholder='NDLS, New Delhi Railway Staion'
+                                    value={departure}
+                                    onChange={(e) => setDeparture(e.target.value)}
+                                />
+                            </Col>
+                            <Col>
+                                <input
+                                    className='inputstyle'
+                                    type="text"
+                                    placeholder='LJN,Lucknow Junction'
+                                    value={destination}
+                                    onChange={(e) => setDestination(e.target.value)}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col><DatePicker className="date" selected={date} onChange={(date) => setDate(date)} dateFormat="MMMM d, yyyy" /></Col>
+                            <Col></Col>
+                        </Row>
+                        <Row><Col><button className="buttonstyle" type="submit" >Search for trains</button></Col></Row>
+
+                    </form>
+
+                </div >
+            </Col>
+
+            <Col >
+
+                <img className="imgStyle" src={metro} alt="metro" />
+
+
+            </Col>
+        </Row>
+
     )
 }
